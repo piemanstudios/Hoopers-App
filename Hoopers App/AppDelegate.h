444f3806c60854693c0b897681d2f55c45ae62b9
Ollie Stephens <Ollie@unknown-00-26-08-e4-27-d7.home>
2011-09-16 10:57:29 +0100

    Initial Commit

---
+++ Hoopers App/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Hoopers App
+//
+//  Created by Ollie on 16/09/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Hoopers App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hoopers App
+//
+//  Created by Ollie on 16/09/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
